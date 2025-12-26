92ab31d20c5ba1690410d18bddc9dfd4ab0c926f
AjHdrr <hyderajmain@gmail.com>
2025-12-26 17:41:28 -0500

    Created components of the CS/Resume page. Working on Hero section rn

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import "../styles/Hero.css";
+
+function Hero () {
+    return (
+        <section className="hero">
+            <h1>AJMAIN HYDER</h1>
+            <p className="text-lg text-gray-600 mb-2">Computer Science Student and Developer</p>
+            <p className="text-gray-600">Toronto Metropolitan University - Co-op Program</p>
+        </section>
+    );
+}
+
+export default Hero;
