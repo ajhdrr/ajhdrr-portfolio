46486686b4aa84c126c30b0bcf508c9aef1a0cf3
AjHdrr <hyderajmain@gmail.com>
2026-01-11 21:03:00 -0500

    Confirmed ReactBits Pixel snow, Changed media

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./styles/App.css"
+import PixelSnow from './PixelSnow';
 
 import Navbar from "./components/CS/Navbar.jsx";
 import Hero from "./components/CS/Hero.jsx";
@@ -13,7 +14,19 @@ import BGimg from "./assets/BGimg.webp"
 
 function App() {
   return (
-    <div className="app" id="CS">   
+    <div className="app" id="CS">  
+       <div style={{ width: '1px', height: '1px', position: 'static' }}>
+        <PixelSnow 
+          color="#ffffff"
+          flakeSize={0.01}
+          minFlakeSize={1.25}
+          pixelResolution={400}
+          speed={1.1}
+          density={0.2}
+          direction={125}
+          brightness={4}
+        />
+      </div>
       <Navbar />
       <Hero />
       <Projects />
