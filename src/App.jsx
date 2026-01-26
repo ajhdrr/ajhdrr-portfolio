b412694594a7b6fa4be4ccc5aff6fa81f90cdab6
AjHdrr <hyderajmain@gmail.com>
2026-01-25 23:10:36 -0500

    changed react router on main and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import "./styles/app.css"
+import { Routes, Route } from "react-router-dom";
 
+import CSPage from "./pages/CSPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div> HOMEPAGE </div>
+    <Routes>
+      <Route path="/" element={<CSPage />} />
+      <Route path="/CS" element={<CSPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
-import './index.css'
-
-import App from './App.jsx'
-import NotFoundPage from './pages/NotFoundPage.jsx'
-import CSPage from './pages/CSPage.jsx'
-
-const router = createBrowserRouter([
-  {path:"/", element: <CSPage />},
-  {path:"/CS", element: <CSPage />},
-  {path:"*", element: <NotFoundPage />}
-]);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
